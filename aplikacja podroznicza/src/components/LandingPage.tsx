a8b816bc266342a6b7af16d46adf8272cfeffdf4
Joanna Okorowska <asia.okorowska@gmail.com>
2023-02-08 14:49:07 +0100

    landingpage

---
+++ aplikacja podroznicza/src/components/LandingPage.tsx
@@ -0,0 +1,8 @@
+import "./LandingPage.css";
+
+export const LandingPage = () => (
+   <>
+   <button className="btn upperBtn">Sign In</button>
+   <button className="btn lowerBtn">Sign Up</button>
+   </>
+  );

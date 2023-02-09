1f279846d8927d3e8c323d079238e909e9b9e789
Joanna Okorowska <asia.okorowska@gmail.com>
2023-02-09 12:13:46 +0100

    Add footer

--- aplikacja podroznicza/src/App.tsx
+++ aplikacja podroznicza/src/App.tsx
@@ -2,17 +2,19 @@ import "./App.css";
 import { Login } from "./components/login-page";
 import { LandingPage } from "./components/LandingPage";
 import { HashRouter, Route, Routes } from "react-router-dom";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <HashRouter>
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signIn" element={<Login />} />
-        </Routes>
-      </div>
-    </HashRouter>
+    // <HashRouter>
+    //   <div className="app">
+    //     <Routes>
+    //       <Route path="/" element={<LandingPage />} />
+    //       <Route path="/signIn" element={<Login />} />
+    //     </Routes>
+    //   </div>
+    // </HashRouter>
+    <Footer/>
   );
 }
 

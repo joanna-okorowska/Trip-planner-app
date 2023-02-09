1e57346b10f991a565302f79c93893fc800b2f77
Sebastian Majda <sebastianmajda90@gmail.com>
2023-02-09 14:40:43 +0100

    Authorization - register and log in is ready

--- aplikacja podroznicza/src/App.tsx
+++ aplikacja podroznicza/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Login } from "./components/login-page";
+import { Register } from "./components/register-page";
 import { LandingPage } from "./components/LandingPage";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { Footer } from "./components/Footer";
@@ -11,10 +12,11 @@ function App() {
     //     <Routes>
     //       <Route path="/" element={<LandingPage />} />
     //       <Route path="/signIn" element={<Login />} />
+    //
     //     </Routes>
     //   </div>
     // </HashRouter>
-    <Footer/>
+    <Login />
   );
 }
 
--- aplikacja podroznicza/src/components/login-page.tsx
+++ aplikacja podroznicza/src/components/login-page.tsx
@@ -16,23 +16,64 @@ import {
   Facebook,
   Fbimg,
 } from "../Styles/login-and-register-page-styled";
+import React, { useContext, useEffect, useState } from "react";
+import { auth } from "../firebase-config";
+import { Navigate } from "react-router";
+import { useNavigate } from "react-router-dom";
+import {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+export function Login(): JSX.Element {
+  const [loginEmail, setLoginEmail] = useState<string>("");
+  const [loginPassword, setLoginPassword] = useState<string>("");
+  const [user, setUser] = useState<string>("");
 
-export function Login() {
+  // onAuthStateChanged(auth, (currentUser) => {
+  //   setUser(currentUser);
+  // });
+
+  const login = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      console.log("user is logged");
+    } catch (error) {
+      console.log("Error accured during login");
+    }
+  };
+  const logout = async () => {
+    await signOut(auth);
+  };
+
   return (
     <div>
       <Global />
       <Container>
         <Logintxt>Sign In To TripTastic:</Logintxt>
-        <form>
+        <form onSubmit={login}>
           <Name>e-mail:</Name>
           <br />
-          <Field></Field>
+          <Field
+            onChange={(event) => {
+              setLoginEmail(event.target.value);
+            }}
+          />
           <br />
           <Name>password:</Name>
           <br />
-          <Field type={"password"}></Field>
+          <Field
+            type={"password"}
+            onChange={(event) => {
+              setLoginPassword(event.target.value);
+            }}></Field>
           <br />
-          <Button type="submit" value="Sign In"></Button>
+          <Button type="submit" value="Sign In" />
         </form>
         <Separator>
           <Line></Line>

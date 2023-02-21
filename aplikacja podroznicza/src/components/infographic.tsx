fd8116ab45111a88e2345186e508a0d798969c6b
Sebastian Majda <sebastianmajda90@gmail.com>
2023-02-21 13:34:27 +0100

    New Inforgraphic img ( issue with background color)

--- aplikacja podroznicza/src/components/infographic.tsx
+++ aplikacja podroznicza/src/components/infographic.tsx
@@ -2,11 +2,14 @@ import {
   InfographicContainer,
   InfographicImage,
 } from "../Styles/infographic-styled";
+import React from "react";
+import InfographicIMG from "../assets/INFO.jpeg";
+
 export const Infographic = () => {
   return (
     <div>
       <InfographicContainer>
-        <InfographicImage />
+        <InfographicImage src={InfographicIMG} alt="Infographic" />
       </InfographicContainer>
     </div>
   );

ed7579e74a17bccd593002c837faaaeddf3ced9b
Sebastian Majda <sebastianmajda90@gmail.com>
2023-02-24 11:12:15 +0100

    video to 100vh

--- aplikacja podroznicza/src/Styles/videoBackground-styled.tsx
+++ aplikacja podroznicza/src/Styles/videoBackground-styled.tsx
@@ -3,7 +3,7 @@ import styled, { createGlobalStyle } from "styled-components";
 export const VideoContainer = styled.div`
   overflow: hidden;
   width: 100vw;
-  height: calc(100vh - 100px);
+  height: 100vh;
   pointer-events: none;
   position: relative;
   margin: 0;
